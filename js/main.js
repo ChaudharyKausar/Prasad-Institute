bed6a481aa8bd44194a05c6e5a687a8899db39e2
Kausar Choudhary <ch.kausar2005@gmail.com>
2025-07-17 16:36:04 +0530

    Develped the blog page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
+
+
 (function ($) {
     "use strict";
     
@@ -64,3 +82,6 @@ let body = document.querySelector("body"),
                 };
             });
         }
+
+
+        
